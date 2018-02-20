0d9c3d7352cafbb945777e5bb738d35eaedf558c
rasjostrom <ras.sjostrom@gmail.com>
2018-02-20 11:55:41 +0100

    Fix coding style, PSR-2

--- library/Theme/Enqueue.php
+++ library/Theme/Enqueue.php
@@ -4,32 +4,38 @@ namespace Simrishamn\Theme;
 
 class Enqueue
 {
-  public function __construct()
-  {
-    // Enqueue scripts and styles
-    add_action('wp_enqueue_scripts', array($this, 'style'));
-    add_action('wp_enqueue_scripts', array($this, 'script'));
-  }
+    public function __construct()
+    {
+        // Enqueue scripts and styles
+        add_action('wp_enqueue_scripts', array($this, 'style'));
+        add_action('wp_enqueue_scripts', array($this, 'script'));
+    }
 
-  /**
-   * Enqueue styles
-   * @return void
-   */
-  public function style()
-  {
-    wp_enqueue_style('Simrishamn-css',
-		     get_stylesheet_directory_uri(). '/assets/dist/css/app.min.css', '',
-		     filemtime(get_stylesheet_directory() . '/assets/dist/css/app.min.css'));
-  }
+    /**
+     * Enqueue styles
+     * @return void
+     */
+    public function style()
+    {
+        wp_enqueue_style(
+            'Simrishamn-css',
+            get_stylesheet_directory_uri(). '/assets/dist/css/app.min.css',
+            '',
+            filemtime(get_stylesheet_directory() . '/assets/dist/css/app.min.css')
+        );
+    }
 
-  /**
-   * Enqueue scripts
-   * @return void
-   */
-  public function script()
-  {
-    wp_enqueue_script('Simrishamn-js',
-		      get_stylesheet_directory_uri(). '/assets/dist/js/app.min.js', '',
-		      filemtime(get_stylesheet_directory() . '/assets/dist/js/app.min.js'), true);
-  }
+    /**
+     * Enqueue scripts
+     * @return void
+     */
+    public function script()
+    {
+        wp_enqueue_script(
+            'Simrishamn-js',
+            get_stylesheet_directory_uri(). '/assets/dist/js/app.min.js', '',
+            filemtime(get_stylesheet_directory() . '/assets/dist/js/app.min.js'),
+            true
+        );
+    }
 }
