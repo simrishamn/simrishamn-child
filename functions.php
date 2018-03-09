c7eb5640d4e5b783dc8b5f4239f0ff6e33dfc900
rasjostrom <ras.sjostrom@gmail.com>
2018-03-09 16:05:38 +0100

    Load custom modules from child theme

--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php
 
 define('SIMRISHAMN_PATH', get_stylesheet_directory() . '/');
+define('CUSTOM_MODULES_PATH', SIMRISHAMN_PATH . 'custom-modules/');
+
+define('DEV_MODE', true);
 
 //Include vendor files
 if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
@@ -12,5 +15,11 @@ $loader = new SIMRISHAMN\Vendor\Psr4ClassLoader();
 $loader->addPrefix('Simrishamn', SIMRISHAMN_PATH . 'library');
 $loader->addPrefix('Simrishamn', SIMRISHAMN_PATH . 'source/php/');
 $loader->register();
+
+if(defined('CUSTOM_MODULES_PATH')) {
+    foreach (glob(CUSTOM_MODULES_PATH . "/*/*.php") as $module) {
+        include $module;
+    }
+}
 
 new Simrishamn\App();
