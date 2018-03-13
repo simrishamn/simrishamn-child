4eebfd56d1ce8e9e963b1d643d087255167eeec4
rasjostrom <ras.sjostrom@gmail.com>
2018-03-13 12:25:09 +0100

    Add arrow and styling to Inlay Index module

--- custom-modules/InlayIndex/views/inlay-index.blade.php
+++ custom-modules/InlayIndex/views/inlay-index.blade.php
@@ -1,16 +1,21 @@
-<div class="{{ $classes }}">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+<div class="{{ $classes }} inlay-index">
   <h4 class="box-title" style="background-color: {{ $title_color }};">
     {!! apply_filters('the_title', $post_title) !!} </h4>
   <div class="box box-horizontal">
     @foreach($items as $item)
-      <div class="box-content" style="background-color: {{ $box_color }};">
-	<a href="">
-	  <span class="title">
-	    {{ $item->post_title }}
-	  </span>
-	  <p style="color: black;">{{ $item->post_content }}</p>
-	</a>
-      </div>
+      <a href="">
+	<div class="box-content" style="background-color: {{ $box_color }};">
+	  <div>
+	    <span class="title">
+	      {{ $item->post_title }}
+	    </span>
+	    <p style="color: black;">{{ $item->post_content }}</p>
+	  </div>
+	  <i class="material-icons">arrow_forward</i>
+	</div>
+      </a>
     @endforeach
   </div>
 </div>
