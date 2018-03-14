295cdb28f48efecaaa4c1cb359b6b10d6f3e1302
rasjostrom <ras.sjostrom@gmail.com>
2018-03-14 15:31:03 +0100

    UpdateAdd template override for Index module

---
+++ templates/module/index.blade.php
@@ -0,0 +1,21 @@
+<div class="grid" data-equal-container>
+    @if (!$hideTitle && !empty($post_title))
+    <div class="grid-xs-12">
+        <h2>{!! apply_filters('the_title', $post_title) !!}</h2>
+    </div>
+    @endif
+
+    @foreach ($items as $item)
+      <div class="{{ $columnClass }}" id="{{ $item['title'] }}">
+        <div>
+	  <h4 class="box-title">{{ $item['title'] }}</h4>
+	  <div class="box-content">
+            {!! $item['lead'] !!}
+	    <a href="{{ $item['permalink'] }}" class="btn btn-plain">
+	      Las Mer
+	    </a>
+	  </div>
+        </div>
+    </div>
+    @endforeach
+</div>
