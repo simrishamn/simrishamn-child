e21b553276fb1d618399863828b812f72fddca42
rasjostrom <ras.sjostrom@gmail.com>
2018-03-20 13:19:49 +0100

    Check if modularity_register_module exists before calling it

--- functions.php
+++ functions.php
@@ -5,9 +5,10 @@ define('CUSTOM_MODULES_PATH', SIMRISHAMN_PATH . 'custom-modules/');
 
 define('DEV_MODE', true);
 
+
 //Include vendor files
 if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
-    include_once dirname(ABSPATH) . '/vendor/autoload.php';
+  include_once dirname(ABSPATH) . '/vendor/autoload.php';
 }
 
 require_once SIMRISHAMN_PATH . 'library/Vendor/Psr4ClassLoader.php';
@@ -16,12 +17,12 @@ $loader->addPrefix('Simrishamn', SIMRISHAMN_PATH . 'library');
 $loader->addPrefix('Simrishamn', SIMRISHAMN_PATH . 'source/php/');
 $loader->register();
 
-if (defined('CUSTOM_MODULES_PATH')) {
-    foreach (glob(CUSTOM_MODULES_PATH . "*") as $module) {
-        modularity_register_module(
-            $module,
-            basename($module)
-        );
-    }
+if (function_exists('modularity_register_module') && defined('CUSTOM_MODULES_PATH')) {
+  foreach (glob(CUSTOM_MODULES_PATH . "*") as $module) {
+    modularity_register_module(
+      $module,
+      basename($module)
+    );
+  }
 }
 new Simrishamn\App();
