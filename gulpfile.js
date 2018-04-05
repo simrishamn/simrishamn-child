3c95909054db425c8d1dc304efbc376dcc997d44
Joakim Soderlund <joakim.soderlund@gmail.com>
2018-04-05 17:29:57 +0200

    Add Browserify and Babel to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,32 @@ var gulp = require('gulp');
 
 // Include Our Plugins
 var autoprefixer = require('gulp-autoprefixer');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
 var concat = require('gulp-concat');
 var cssnano = require('gulp-cssnano');
 var gettext = require('gulp-gettext');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
 
 // Compile translations.
-gulp.task('po-dist', function() {
+gulp.task('lang', function() {
     return gulp.src('languages/*.po')
         .pipe(gettext())
         .pipe(gulp.dest('languages'));
 });
 
-// Compile Our Sass
-gulp.task('sass-dist', function() {
+// Compile application styles.
+gulp.task('style', function() {
     return gulp.src('assets/source/sass/app.scss')
         .pipe(sass())
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-        .pipe(rename({suffix: '.min'}))
+        .pipe(rename('app.dev.css'))
+        .pipe(gulp.dest('assets/dist/css'))
+        .pipe(rename('app.min.css'))
         .pipe(cssnano({
             mergeLonghand: false,
             zindex: false
@@ -30,31 +36,27 @@ gulp.task('sass-dist', function() {
         .pipe(gulp.dest('assets/dist/css'));
 });
 
-gulp.task('sass-dev', function() {
-    return gulp.src('assets/source/sass/app.scss')
-        .pipe(sass())
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-        .pipe(rename({suffix: '.dev'}))
-        .pipe(gulp.dest('assets/dist/css'));
-});
-
-// Concatenate & Minify JS
-gulp.task('scripts-dist', function() {
-    return gulp.src('assets/source/js/*.js')
-        .pipe(concat('app.js'))
+// Compile application scripts.
+gulp.task('script', function() {
+    return browserify({entries: 'assets/source/js/app.js'})
+        .transform(babelify, {presets: ['env']})
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(buffer())
+        .pipe(rename('app.dev.js'))
         .pipe(gulp.dest('assets/dist/js'))
         .pipe(rename('app.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('assets/dist/js'));
 });
 
-// Watch Files For Changes
+// File change watches.
 gulp.task('watch', function() {
-    gulp.watch('assets/source/js/**/*.js', ['scripts-dist']);
-    gulp.watch('assets/source/sass/**/*.scss', ['sass-dist', 'sass-dev']);
-    gulp.watch('languages/*.po', ['po-dist']);
+    gulp.watch('assets/source/js/**/*.js', ['script']);
+    gulp.watch('assets/source/sass/**/*.scss', ['style']);
+    gulp.watch('languages/*.po', ['lang']);
 });
 
-// Default Task
-gulp.task('build', ['po-dist', 'sass-dist', 'sass-dev', 'scripts-dist']);
+// Command line tasks.
+gulp.task('build', ['lang', 'script', 'style']);
 gulp.task('default', ['build', 'watch']);
