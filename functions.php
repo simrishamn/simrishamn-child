e3def9852651925fcd5fae289bf465ceedb65ff2
Joakim Soderlund <joakim.soderlund@gmail.com>
2018-04-10 15:33:40 +0200

    Clean up functions file slightly

--- functions.php
+++ functions.php
@@ -2,15 +2,16 @@
 
 define('SIMRISHAMN_PATH', get_stylesheet_directory());
 
-//Include vendor files
+// Include vendor files.
 if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
     include_once dirname(ABSPATH) . '/vendor/autoload.php';
 }
 
+// Initialize class loader.
 require_once SIMRISHAMN_PATH . '/library/Vendor/Psr4ClassLoader.php';
 $loader = new SIMRISHAMN\Vendor\Psr4ClassLoader();
 $loader->addPrefix('Simrishamn', SIMRISHAMN_PATH . '/library');
-$loader->addPrefix('Simrishamn', SIMRISHAMN_PATH . '/source/php/');
 $loader->register();
 
+// Run app class.
 new Simrishamn\App();
