bdb1222a1ab89a0481438c0d8adc5130bf89b401
rasjostrom <ras.sjostrom@gmail.com>
2018-04-10 14:43:00 +0200

    Change layout in archive template & remove hidden on right sidebar
    - Content section is ordered last when screen-below-lg
    - Filter section is placed above content and takes up full width
    - Right sidebar is no longer hidden on small devices

---
+++ views/partials/sidebar-right.blade.php
@@ -0,0 +1,14 @@
+@if ($hasRightSidebar)
+<aside class="grid-lg-3 grid-md-12 sidebar-right-sidebar">
+    @if (is_active_sidebar('right-sidebar') || (isset($enabledSidebarFilters) && is_array($enabledSidebarFilters)))
+    <div class="grid">
+
+        @if (isset($enabledSidebarFilters) && is_array($enabledSidebarFilters))
+            @include('partials.blog.taxonomy-filters')
+        @endif
+
+        <?php dynamic_sidebar('right-sidebar'); ?>
+    </div>
+    @endif
+</aside>
+@endif
