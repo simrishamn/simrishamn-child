a1ac52be4afc279bac793b16fcbe00077907349c
Joakim Soderlund <joakim.soderlund@gmail.com>
2018-04-10 15:26:07 +0200

    Fixed broken paths in functions file

--- functions.php
+++ functions.php
@@ -7,10 +7,10 @@ if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
     include_once dirname(ABSPATH) . '/vendor/autoload.php';
 }
 
-require_once SIMRISHAMN_PATH . 'library/Vendor/Psr4ClassLoader.php';
+require_once SIMRISHAMN_PATH . '/library/Vendor/Psr4ClassLoader.php';
 $loader = new SIMRISHAMN\Vendor\Psr4ClassLoader();
-$loader->addPrefix('Simrishamn', SIMRISHAMN_PATH . 'library');
-$loader->addPrefix('Simrishamn', SIMRISHAMN_PATH . 'source/php/');
+$loader->addPrefix('Simrishamn', SIMRISHAMN_PATH . '/library');
+$loader->addPrefix('Simrishamn', SIMRISHAMN_PATH . '/source/php/');
 $loader->register();
 
 new Simrishamn\App();
