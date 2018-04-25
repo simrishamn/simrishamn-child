d753fb619d47c740aa41c5f1586c4083ef5d16e8
rasjostrom <ras.sjostrom@gmail.com>
2018-04-25 16:16:57 +0200

    Fix issue with margins & add text box for news items

--- custom-modules/NewsList/views/news-grid.blade.php
+++ custom-modules/NewsList/views/news-grid.blade.php
@@ -1,43 +1,50 @@
 <div class="grid news-grid">
-  <div class="grid-md-6">
-    <a href="{{ get_permalink($featured[0]->ID) }}"
-       class="box box-post-brick">
-      <div class="box-image"
-           style="background-image:url({{ $featured[0]->thumbnail }});">
-        <img src="{{ $featured[0]->thumbnail }}"
-             alt="{{ $featured[0]->post_title }}">
-      </div>
-    </a>
-  </div>
-  <div class="grid-md-6">
-    <div class="grid">
-      @foreach($items as $item)
-	<div class="grid-md-6">
-	  <a href="{{ get_permalink($item->ID) }}"
-             class="box box-post-brick" style="">
+    <div class="grid-md-6">
+	<a href="{{ get_permalink($featured[0]->ID) }}"
+	   class="box box-post-brick">
 	    <div class="box-image"
-		 style="background-image:url({{ $item->thumbnail }});">
-              <img src="{{ $item->thumbnail }}"
-		   alt="{{ $item->post_title }}">
+		 style="background-image:url({{ $featured[0]->thumbnail }});">
+		<img src="{{ $featured[0]->thumbnail }}"
+		     alt="{{ $featured[0]->post_title }}">
+		<div class="grid-item-content">
+		    <h6 class="">Yvonnes Testnyhet</h6>
+		    {{ $featured[0]->post_excerpt }}
+		</div>
 	    </div>
-	  </a>
-	</div>
-      @endforeach
-      <div class="grid-md-6">
-	<div class="box box-post-brick index-content title-{{$content_color}}">
-	  <div class="brick-container">
-	    <p>
-	      Hitta fler hyter i Nyhetsarkivet!
-	      Lättast är att klicka på Gå Till knappen.
-	    </p>
-	    <div class="btn-holder">
-	      <a class="btn btn-plain" href="{{ get_post_type_archive_link( 'post' ) }}">
-          Gå Till
-	      </a>
-	    </div>
-	  </div>
-	</div>
-      </div>
+	</a>
     </div>
-  </div>
+    <div class="grid-md-6">
+	<div class="grid">
+	    @foreach($items as $item)
+		<div class="grid-md-6">
+		    <a href="{{ get_permalink($item->ID) }}"
+		       class="box box-post-brick" style="">
+			<div class="box-image"
+			     style="background-image:url({{ $item->thumbnail }});">
+			    <img src="{{ $item->thumbnail }}"
+				 alt="{{ $item->post_title }}">
+			    <div class="grid-item-content">
+				<h6>Yvonnes Testnyhet</h6>
+			    </div>
+			</div>
+		    </a>
+		</div>
+	    @endforeach
+	    <div class="grid-md-6">
+		<div class="box box-post-brick index-content title-{{$content_color}}">
+		    <div class="brick-container">
+			<p>
+			    Hitta fler hyter i Nyhetsarkivet!
+			    Lättast är att klicka på Gå Till knappen.
+			</p>
+			<div class="btn-holder">
+			    <a class="btn btn-plain" href="{{ get_post_type_archive_link( 'post' ) }}">
+				Gå Till
+			    </a>
+			</div>
+		    </div>
+		</div>
+	    </div>
+	</div>
+    </div>
 </div>
