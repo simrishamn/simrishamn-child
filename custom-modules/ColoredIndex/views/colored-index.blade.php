2fa5532afd1b14f6420ef77dca7d6afd3b21b6e5
rasjostrom <ras.sjostrom@gmail.com>
2018-05-21 15:21:43 +0200

    Fix indentation in Colored index Index view

--- custom-modules/ColoredIndex/views/colored-index.blade.php
+++ custom-modules/ColoredIndex/views/colored-index.blade.php
@@ -1,18 +1,18 @@
 <div class="grid" data-equal-container>
-  @foreach($items as $item)
-    <div class="{{$columnClass}} box-panel">
-      <div class="index-content content-{{$item['color']}}">
-	<h4 class="box-title title-{{$item['color']}}">{{$item['title']}}</h4>
-	@if($item['image-p'])
-	  <div>
-	    <img src="{{$item['image']}}" style="max-height: 165px; max-width: 300px;"/>
-	  </div>
-	@endif
-	<p>{{ $item['lead'] }}</p>
-	<div class="btn-holder">
-	  <a href={{ $item['link_url'] }} class="btn btn-plain">{{$item['btn-text']}}</a>
+    @foreach($items as $item)
+	<div class="{{$columnClass}} box-panel">
+	    <div class="index-content content-{{$item['color']}}">
+		<h4 class="box-title title-{{$item['color']}}">{{$item['title']}}</h4>
+		@if($item['image-p'])
+		    <div>
+			<img src="{{$item['image']}}" style="max-height: 165px; max-width: 300px;"/>
+		    </div>
+		@endif
+		<p>{{ $item['lead'] }}</p>
+		<div class="btn-holder">
+		    <a href={{ $item['link_url'] }} class="btn btn-plain">{{$item['btn-text']}}</a>
+		</div>
+	    </div>
 	</div>
-      </div>
-    </div>
     @endforeach
 </div>
