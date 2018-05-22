20c5a18a7dfd8a89c9236d9091a93fc6128173e8
rasjostrom <ras.sjostrom@gmail.com>
2018-05-22 17:32:23 +0200

    Blacklist all relevant module areas by default and whitelist specific modules on a template basis

--- library/Theme/Modularity.php
+++ library/Theme/Modularity.php
@@ -15,7 +15,160 @@ class Modularity
             return;
         }
 
+        add_filter(
+            'Modularity/Init',
+            array($this, 'modulePageRestriction'),
+            20,
+            2
+        );
+
+        add_filter(
+            'Modularity/Editor/SidebarIncompability',
+            array($this, 'moduleIncompatibility'),
+            20,
+            2
+        );
+
         $this->load();
+    }
+
+    public function modulePageRestriction()
+    {
+        $enabledModules = array();
+        switch(\Modularity\Helper\Post::getPostTemplate()) {
+        case "front-page":
+            array_push(
+                $enabledModules,
+                'mod-colored-index',
+                'mod-linklist',
+                'mod-inlay-index',
+                'mod-image',
+                'mod-slider',
+                'mod-action-card',
+                'mod-news-list'
+            );
+            break;
+        case "full-width.blade.php":
+            array_push(
+                $enabledModules,
+                'mod-image',
+                'mod-colored-index',
+                'mod-contacts',
+                'mod-linklist',
+                'mod-news-list'
+            );
+            break;
+        case "page":
+            array_push(
+                $enabledModules,
+                'mod-contacts',
+                'mod-colored-index',
+                'mod-linklist',
+                'mod-image',
+                'mod-slider',
+                'mod-form',
+                'mod-iframe',
+                'mod-fileslist'
+            );
+            break;
+        case "single":
+            array_push(
+                $enabledModules,
+                'mod-contacts',
+                'mod-contacts-fixed',
+                'mod-linklist',
+                'mod-news-list',
+                'mod-fileslist'
+            );
+            break;
+        }
+
+        \Modularity\ModuleManager::$enabled = $enabledModules;
+    }
+
+    public function moduleIncompatibility($moduleSpecification, $modulePostType)
+    {
+        $template = \Modularity\Helper\Post::getPostTemplate();
+        $sidebarIncompatibilities = array(
+            "right-sidebar" => true,
+            "content-area-top" => true,
+            "content-area" => true,
+            "bottom-sidebar" => true,
+            "slider-area" => true
+        );
+
+        switch ($modulePostType) {
+        case "mod-colored-index":
+            if ($template == 'page') {
+                unset($sidebarIncompatibilities['right-sidebar']);
+                unset($sidebarIncompatibilities['content-area']);
+            } elseif ($template =='full-width.blade.php') {
+                unset($sidebarIncompatibilities['content-area']);
+            }
+            break;
+        case "mod-contacts":
+            if ($template == 'page' || $template == 'single') {
+                unset($sidebarIncompatibilities['right-sidebar']);
+            } elseif ($template =='full-width.blade.php') {
+                unset($sidebarIncompatibilities['content-area']);
+            }
+            break;
+        case "mod-linklist":
+            if ($template == 'page') {
+                unset($sidebarIncompatibilities['right-sidebar']);
+            }
+            unset($sidebarIncompatibilities['content-area']);
+            break;
+        case "mod-image":
+            if ($template == 'page') {
+                unset($sidebarIncompatibilities['content-area-top']);
+            } elseif ($template =='full-width.blade.php') {
+                unset($sidebarIncompatibilities['slider-area']);
+            }
+            break;
+        case "mod-slider":
+            if ($template == 'page') {
+                unset($sidebarIncompatibilities['content-area-top']);
+            }
+            break;
+        case "mod-form":
+            if ($template == 'page') {
+                unset($sidebarIncompatibilities['content-area']);
+            }
+            break;
+        case "mod-iframe":
+            if ($template == 'page') {
+                unset($sidebarIncompatibilities['content-area']);
+            }
+            break;
+        case "mod-fileslist":
+            if ($template == 'page' || $template == 'single') {
+                unset($sidebarIncompatibilities['content-area']);
+            }
+            break;
+        case "mod-posts-expand":
+            if ($template == 'page') {
+                unset($sidebarIncompatibilities['content-area']);
+            }
+            break;
+        case "mod-news-list":
+            if ($template == 'full-width.blade.php') {
+                unset($sidebarIncompatibilities['bottom-sidebar']);
+            } elseif ($template == 'single') {
+                unset($sidebarIncompatibilities['right-sidebar']);
+            }
+            break;
+        case "mod-contacts-fixed":
+            if ($template == 'page' || $template == 'single') {
+                unset($sidebarIncompatibilities['right-sidebar']);
+            } elseif ($template == 'full-width.blade.php') {
+                unset($sidebarIncompatibilities['content-area']);
+            }
+            break;
+        }
+
+        $moduleSpecification['sidebar_incompability'] = array_keys($sidebarIncompatibilities);
+        return $moduleSpecification;
     }
 
     /**
