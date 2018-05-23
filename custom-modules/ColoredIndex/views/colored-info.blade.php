03b1a9934cc97aa3f9d483bd347d1d33c722865f
OLFR8460 <ola.franzen@sigma.se>
2018-05-23 21:34:50 +0200

    Fixed paddings/margins in modules in sidebar bottom and right (hopefully not messing other things up). Made truncated links not overflow on hover. Fix for colored index and info modules to show correctly with box

--- custom-modules/ColoredIndex/views/colored-info.blade.php
+++ custom-modules/ColoredIndex/views/colored-info.blade.php
@@ -1,11 +1,13 @@
 <div class="grid" data-equal-container>
   @foreach($items as $item)
-    <div class="{{$columnClass}} box-panel index-info">
-      <div class="index-content title-{{$item['color']}}">
-	<p>{{ $item['lead'] }}</p>
-	<div class="btn-holder">
-	  <a href={{ $item['link_url'] }} class="btn btn-plain">{{$item['btn-text']}}</a>
-	</div>
+    <div class="{{$columnClass}}">
+      <div class="box box-panel index-info">
+        <div class="index-content title-{{$item['color']}}">
+          <p>{{ $item['lead'] }}</p>
+          <div class="btn-holder">
+            <a href={{ $item['link_url'] }} class="btn btn-plain">{{$item['btn-text']}}</a>
+          </div>
+        </div>
       </div>
     </div>
   @endforeach
