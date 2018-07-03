369128c8426c1a98c7fe20a10ada207e3b52a96a
Joakim Soderlund <joakim.soderlund@gmail.com>
2018-07-03 11:36:01 +0200

    NOJIRA Improve readability of TinyMCE Locale

--- library/Theme/Language/TinyMceLocale.php
+++ library/Theme/Language/TinyMceLocale.php
@@ -1,8 +1,23 @@
 <?php
-$strings = 'tinyMCE.addI18n(
-    \'' . $mce_locale . '.infobox\',
-    {
-        titleText: \'' . esc_js(_x('Title', 'Default text shown in the infobox header upon creation', 'simrishamn')) . '\',
-        contentText: \'' . esc_js(_x('Content', 'Default text shown in the infobox content paragraph upon creation', 'simrishamn')) . '\',
-        buttonTitle: \'' . esc_js(_x('Infobox', 'Tooltip text for the infobox button', 'simrishamn')) . '\'
-    });';
+
+function serialize_translations($data)
+{
+    $strings = array();
+
+    foreach ($data as $key => $value) {
+        $key = wp_json_encode($key);
+        $value = wp_json_encode($value);
+        $string = "tinyMCE.addI18n($key, $value);";
+        array_push($strings, $string);
+    }
+
+    return implode("\n", $strings);
+}
+
+$strings = serialize_translations(array(
+    "$mce_locale.infobox" => array(
+        'titleText' => _x('Title', 'Default text shown in the infobox header upon creation', 'simrishamn'),
+        'contentText' => _x('Content', 'Default text shown in the infobox content paragraph upon creation', 'simrishamn'),
+        'buttonTitle' => _x('Infobox', 'Tooltip text for the infobox button', 'simrishamn'),
+    ),
+));
