7194ccb3325fbf0aa2c7f28a2cf7756d09e38e2d
Magdalena Korpela <magdalena.osbakk@gmail.com>
2018-08-10 16:01:15 +0200

    Add logo to log in page, to choose logo to be shown go inte Theme Customize > Site Identity and choose a logo

--- library/Theme/Login.php
+++ library/Theme/Login.php
@@ -10,15 +10,37 @@ class Login
      */
     public function __construct()
     {
-        add_action( 'login_enqueue_scripts', [$this, 'my_login_stylesheet']);
-    
-        //add_action( 'login_enqueue_scripts', 'my_login_logo' );
+        add_action('login_enqueue_scripts', [$this, 'myLoginStylesheet']);
+        add_action('after_setup_theme', [$this, 'themePrefixSetup']);
+        add_action('login_head', [$this, 'replaceLogo'], 100);
     }
     
+    public function myLoginStylesheet()
+    {
+        wp_enqueue_style('custom-login', get_stylesheet_directory_uri() . '/assets/dist/css/login.dev.css');
+    }
     
-    public function my_login_stylesheet() {
-        wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/assets/dist/css/login.dev.css');
-        //wp_enqueue_script( 'custom-login', get_stylesheet_directory_uri() . '/login.js' );
+    public function themePrefixSetup()
+    {
+        add_theme_support('custom-logo');
     }
-
+    
+    public function replaceLogo()
+    {
+        if (has_custom_logo()) :
+            $image = wp_get_attachment_image_src(get_theme_mod('custom_logo'), 'full');
+            
+            ?>
+            <style type="text/css">
+                .login h1 a {
+                    background-image: url(<?php echo esc_url($image[0]); ?>);
+                    -webkit-background-size: <?php echo absint($image[1])?>px;
+                    background-size: <?php echo absint($image[1]) ?>px;
+                    height: <?php echo absint($image[2]) ?>px;
+                    width: <?php echo absint($image[1]) ?>px;
+                }
+            </style>
+            <?php
+        endif;
     }
+}
