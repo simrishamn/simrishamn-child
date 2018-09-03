5945ab048478975b87c70cf61f7661200cc880d6
Joakim Soderlund <joakim.soderlund@gmail.com>
2018-09-03 14:22:22 +0200

    NOJIRA Output mix assets to directories

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,11 +12,11 @@ const config = {
 };
 
 Mix.webpackConfig(config)
-    .js('assets/source/js/admin.js', 'assets/dist/js/admin.js')
-    .js('assets/source/js/editor.js', 'assets/dist/js/editor.js')
-    .js('assets/source/js/helpers.js', 'assets/dist/js/helpers.js')
-    .js('assets/source/js/theme.js', 'assets/dist/js/theme.js')
-    .sass('assets/source/sass/admin.scss', 'assets/dist/css/admin.css')
-    .sass('assets/source/sass/editor.scss', 'assets/dist/css/editor.css')
-    .sass('assets/source/sass/theme.scss', 'assets/dist/css/theme.css')
+    .js('assets/source/js/admin.js', 'assets/dist/js')
+    .js('assets/source/js/editor.js', 'assets/dist/js')
+    .js('assets/source/js/helpers.js', 'assets/dist/js')
+    .js('assets/source/js/theme.js', 'assets/dist/js')
+    .sass('assets/source/sass/admin.scss', 'assets/dist/css')
+    .sass('assets/source/sass/editor.scss', 'assets/dist/css')
+    .sass('assets/source/sass/theme.scss', 'assets/dist/css')
     .sourceMaps();
