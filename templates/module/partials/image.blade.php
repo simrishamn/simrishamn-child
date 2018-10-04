56681cca89ba3923930bf3560cae95652e7701d6
rasjostrom <ras.sjostrom@gmail.com>
2018-10-04 16:32:56 +0200

    SIEW-213 Adjust hero section to new design
    Changes in mod-slider overlay:
    - Move text overlay to bottom
    - Remove ability to position overlay on the left side
    - Remove fixed logo
    - Limit max-width of overlay text
    - Increase opacity

--- templates/module/partials/image.blade.php
+++ templates/module/partials/image.blade.php
@@ -1,5 +1,8 @@
 @if ($slide['image_use'] !== false)
-<div class="slider-image slider-image-desktop {{ apply_filters('Modularity/slider/desktop_image_hidden', 'hidden-xs hidden-sm') }}" style="background-image:url({{ ($slide['image_use'] !== false) ? $slide['image_use'][0] : '' }})">
+<div class="slider-image
+            slider-image-desktop
+            {{ apply_filters('Modularity/slider/desktop_image_hidden', 'hidden-xs hidden-sm') }}"
+     style="background-image:url({{ ($slide['image_use'] !== false) ? $slide['image_use'][0] : '' }})">
 	<!-- Text block -->
 	@if (isset($slide['activate_textblock']) && $slide['activate_textblock'])
 	@include('partials.textblock')'
--- templates/module/partials/textblock.blade.php
+++ templates/module/partials/textblock.blade.php
@@ -1,24 +1,17 @@
 <span class="text-block
-	     {{ isset($slide['textblock_position']) &&
-		$slide['textblock_position'] == 'center' ? 'text-block-center' : '' }}
-	     {{ isset($slide['textblock_title']) &&
-		strlen($slide['textblock_title']) > 0 ||
-		isset($slide['textblock_content']) &&
-		strlen($slide['textblock_content']) > 0  ? '' : 'text-block-transparent' }}">
-    @if (is_front_page())
-	<div class="">
-	    <img src="{{ get_field('slider_logo', 'option') }}"
-		 alt=""
-		 class="block-level image-responsive"/>
-	</div>
-    @endif
-    <span>
+             {{ isset($slide['textblock_position']) &&
+                $slide['textblock_position'] == 'center' ? 'text-block-center' : '' }}
+             {{ isset($slide['textblock_title']) &&
+                strlen($slide['textblock_title']) > 0 ||
+                isset($slide['textblock_content']) &&
+                strlen($slide['textblock_content']) > 0  ? '' : 'text-block-transparent' }}">
+    <div class="text-block-content">
         @if (isset($slide['textblock_title']) && strlen($slide['textblock_title']) > 0)
             <em class="title text-xl block-level">{!! do_shortcode($slide['textblock_title']) !!}</em>
         @endif
 
         @if (isset($slide['textblock_content']) && strlen($slide['textblock_content']) > 0)
-            {!! do_shortcode($slide['textblock_content']) !!}
+            <p>{!! do_shortcode($slide['textblock_content']) !!}</p>
        @endif
-    </span>
+    </div>
 </span>
