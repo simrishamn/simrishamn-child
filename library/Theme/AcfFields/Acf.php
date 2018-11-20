409a4bebc8de1840d9cdc79a4ab04bc0b9f64815
rasjostrom <ras.sjostrom@gmail.com>
2018-11-20 14:38:31 +0100

    SIEW-265 Add mod-slider field override

--- library/Theme/AcfFields/Acf.php
+++ library/Theme/AcfFields/Acf.php
@@ -9,6 +9,7 @@ class Acf
     public function __construct()
     {
         add_action('after_setup_theme', array($this, 'includeThemeOptions'), 10, 3);
+        add_action('after_setup_theme', array($this, 'includeModOverrides'), 10, 3);
     }
 
     /**
@@ -19,6 +20,18 @@ class Acf
     public function includeThemeOptions()
     {
         foreach (glob($this->_path . '/theme-options/*') as $module) {
+            include_once $module;
+        }
+    }
+
+    /**
+     * Include Modularity ACF Overrides.
+     *
+     * @return void
+     */
+    public function includeModOverrides()
+    {
+        foreach (glob($this->_path . '/modularity/*') as $module) {
             include_once $module;
         }
     }
