d0d51ea06384081518f3a4441a04a8f32c96fe5f
Joakim Soderlund <joakim.soderlund@gmail.com>
2019-02-19 16:14:34 +0100

    NOJIRA Use relative names in Application class

--- library/Theme/Application.php
+++ library/Theme/Application.php
@@ -5,16 +5,16 @@ class Application
 {
     public function __construct()
     {
-        new \Simrishamn\Theme\AdminBar();
-        new \Simrishamn\Theme\Customizer();
-        new \Simrishamn\Theme\DefaultModules();
-        new \Simrishamn\Theme\Enqueue();
-        new \Simrishamn\Theme\Language();
-        new \Simrishamn\Theme\AcfFields();
-        new \Simrishamn\Theme\Modularity();
-        new \Simrishamn\Theme\Style();
-        new \Simrishamn\Theme\Template();
-        new \Simrishamn\Theme\TinyMCE();
-        new \Simrishamn\Theme\GlobalNotice();
+        new AdminBar();
+        new Customizer();
+        new DefaultModules();
+        new Enqueue();
+        new Language();
+        new AcfFields();
+        new Modularity();
+        new Style();
+        new Template();
+        new TinyMCE();
+        new GlobalNotice();
     }
 }
