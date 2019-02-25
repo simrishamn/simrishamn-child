ff3ebd511e79c2f151c47f0dd5447188df15d570
Joakim Soderlund <joakim.soderlund@gmail.com>
2019-02-25 17:35:22 +0100

    NOJIRA Set Kirki URL to global vendor directory

--- library/Theme/Customizer.php
+++ library/Theme/Customizer.php
@@ -6,6 +6,7 @@ class Customizer
     public function __construct()
     {
         add_action('after_setup_theme', [$this, 'enableCustomizer'], 5);
+        add_filter('kirki/config', [$this, 'configureKirki'], 20);
     }
 
     public function enableCustomizer()
@@ -17,5 +18,21 @@ class Customizer
         add_filter('Municipio/Controller/BaseController/Customizer', $enable);
         add_filter('Municipio/Theme/Enqueue/Bem', $enable);
         add_filter('Municipio/Widget/Widgets/CustomizerWidgets', $enable);
+    }
+
+    public function configureKirki($config)
+    {
+        $root = $_SERVER['DOCUMENT_ROOT'];
+        $path = 'vendor/aristath/kirki/';
+
+        if (!is_array($config)) {
+            $config = [];
+        }
+
+        if ($root && file_exists("$root/$path")) {
+            $config['url_path'] = get_home_url(null, $path);
+        }
+
+        return $config;
     }
 }
