d1375f411023bb5f502ea35e669ccd4136cb0d2c
Patrik Björn <patrik.bjorn@sigma.se>
2019-03-07 13:34:26 +0100

    SIEW-272 Re-added vendor include

--- functions.php
+++ functions.php
@@ -2,6 +2,11 @@
 
 define('SIMRISHAMN_PATH', get_stylesheet_directory());
 
+// Include vendor files.
+if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
+    include_once dirname(ABSPATH) . '/vendor/autoload.php';
+}
+
 // Initialize class loader.
 require_once SIMRISHAMN_PATH . '/library/Vendor/Psr4ClassLoader.php';
 $loader = new SIMRISHAMN\Vendor\Psr4ClassLoader();
