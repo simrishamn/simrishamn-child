9955701b2d58cbc9d3a090c94b7a317ed78e2fbd
Joakim Soderlund <joakim.soderlund@gmail.com>
2019-03-21 17:21:51 +0100

    Apply linter fixes

--- library/Theme/CustomModuleHelper.php
+++ library/Theme/CustomModuleHelper.php
@@ -4,69 +4,66 @@ namespace Simrishamn\Theme;
 
 class CustomModuleHelper
 {
-    
     const DOMAIN        = 'simrishamn';
     const MODULE_FOLDER = SIMRISHAMN_PATH . '/custom-modules/';
-    
+
     /**
      * Initialize the Module
      *
      * @param object $object Current Class object
      * @return object The module settings
      */
-    public static function setModule ($object) : object
+    public static function setModule($object) : object
     {
-        
-        if (!$object)
+        if (!$object) {
             throw new \Exception('No object was found.');
-        
+        }
+
         $obj = new \stdClass();
-        
+
         $obj->moduleName    = self::moduleName($object);
-        
+
         $obj->slug          = self::kebabcase($obj->moduleName);
         $obj->fields        = self::fields($obj->moduleName);
-        
-        if (file_exists($obj->fields))
+
+        if (file_exists($obj->fields)) {
             include_once $obj->fields;
-        
+        }
+
         return $obj;
-        
     }
-    
+
     /**
      * Current Class name
      *
      * @param string $object Current Class object
      * @return string The module name
      */
-    public static function moduleName ($object) : string
+    public static function moduleName($object) : string
     {
-        
         return (new \ReflectionClass($object))->getShortName();
-        
     }
-    
+
     /**
      * Creates a slugified kebabcase string
      *
      * @param string $string The string to be converted
      * @return string The converted string
      */
-    public static function kebabcase ($string) : string
+    public static function kebabcase($string) : string
     {
-        
         $string = strip_tags($string);
-        
+
         $string = preg_replace('~[^\pL\d]+~u', '-', $string);
-        
+
         setlocale(LC_MONETARY, get_locale());
-        
+
         $locale = localeconv();
-        
-        if ($locale['int_curr_symbol'] == "")
+
+        if ($locale['int_curr_symbol'] == "") {
             setlocale(LC_MONETARY, 'sv_SE.utf8');
-        
+        }
+
         $string = iconv('utf-8', 'us-ascii//TRANSLIT', $string);
 
         $string = preg_replace('~[^-\w]+~', '', $string);
@@ -74,18 +71,18 @@ class CustomModuleHelper
         $string = trim($string, '-');
 
         $string = preg_replace('~-+~', '-', $string);
-        
+
         $string = preg_replace(['/([a-z\d])([A-Z])/', '/([^-])([A-Z][a-z])/'], '$1-$2', $string);
-        
+
         $string = strtolower($string);
 
-        if (empty($string))
+        if (empty($string)) {
             throw new \Exception('No slug was set.');
-        
+        }
+
         return $string;
-        
     }
-    
+
     /**
      * The full url to the fields php-file
      *
@@ -93,18 +90,17 @@ class CustomModuleHelper
      * @param mixed $customModuleName Set custom filename, always prefixed with mod-
      * @return string The php-file url
      */
-    public static function fields ($moduleName, $customModuleModName = false) : string
+    public static function fields($moduleName, $customModuleModName = false) : string
     {
-        
-        if($customModuleModName)
+        if ($customModuleModName) {
             $moduleModName = $customModuleModName;
-        else
+        } else {
             $moduleModName = self::kebabcase($moduleName);
-        
+        }
+
         return self::MODULE_FOLDER . $moduleName . '/acf/php/mod-' . $moduleModName . '.php';
-        
     }
-    
+
     /**
      * Module classes
      *
@@ -112,11 +108,8 @@ class CustomModuleHelper
      * @param object $object Current Class object
      * @return string The classes in a string
      */
-    public static function classes ($classes, $object) : string
+    public static function classes($classes, $object) : string
     {
-        
         return implode(' ', apply_filters('Modularity/Module/Classes', $classes, $object->post_type, $object->args));
-        
     }
-    
 }
