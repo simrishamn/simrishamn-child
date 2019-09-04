7335d57572bb83c3b7962ac6f53e9dd886aa457d
rasjostrom <ras.sjostrom@gmail.com>
2019-09-04 16:13:56 +0200

    SIEW-292 Wrap primary- and mobile menu in sticky-scroll container

--- bem-views/partials/header/customizer.blade.php
+++ bem-views/partials/header/customizer.blade.php
@@ -1,7 +1,7 @@
 @extends('partials.header')
 
 @section('before-header-body')
-    @include('partials.navigation.search-top')
+
     <div class="print-only container">
         <div class="grid">
             <div class="grid-sm-12">
@@ -12,25 +12,39 @@
 @stop
 
 @section('after-header-body')
-    <nav id="mobile-menu" class="nav-mobile-menu nav-toggle nav-toggle-expand {!! apply_filters('Municipio/mobile_menu_breakpoint','hidden-md hidden-lg'); !!} hidden-print">
-        @include('partials.mobile-menu')
-    </nav>
+
 @stop
 
 @section('header-body')
     @if (isset($headerLayout['headers']) && is_array($headerLayout['headers']) && !empty($headerLayout['headers']))
         @foreach ($headerLayout['headers'] as $header)
             @if ($header->sidebar)
-                <div {!! $header->wrapper !!}>
-                    <div {!! $header->container !!}>
-                        <div {!! $header->grid !!}>
-                            <?php dynamic_sidebar($header->sidebar); ?>
+                @if ($header->sidebar == "customizer-header-primary")
+                    <div class="sticky-scroll">
+                        <div {!! $header->wrapper !!}>
+                            <div {!! $header->container !!}>
+                                <div {!! $header->grid !!}>
+                                    <?php dynamic_sidebar($header->sidebar); ?>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
+                @else
+                    <div {!! $header->wrapper !!}>
+                        <div {!! $header->container !!}>
+                            <div {!! $header->grid !!}>
+                                <?php dynamic_sidebar($header->sidebar); ?>
+                            </div>
+                        </div>
+                    </div>
+                @endif
             @endif
         @endforeach
+        @include('partials.navigation.search-top')
     @endif
+    <nav id="mobile-menu" class="nav-mobile-menu nav-toggle nav-toggle-expand {!! apply_filters('Municipio/mobile_menu_breakpoint','hidden-md hidden-lg'); !!} hidden-print">
+        @include('partials.mobile-menu')
+    </nav>
 @stop
 
 @section('below-header')
