c17e9fb1e8e2c1cc392f7bbf787a3a2e15118873
Sabina Hall <Sabina.Hall@sigma.se>
2020-08-28 08:58:20 +0200

    WCAG Fixed problem regarding link destination of "skip to main-content" missing.

--- bem-views/front-page.blade.php
+++ bem-views/front-page.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     @if (is_active_sidebar('content-area'))
-        <section id="main-content" class="creamy creamy-border-bottom gutter-xl gutter-vertical sidebar-content-area">
+        <section class="creamy creamy-border-bottom gutter-xl gutter-vertical sidebar-content-area">
             <div class="container">
                 <div class="grid">
                     @php dynamic_sidebar('content-area'); @endphp
