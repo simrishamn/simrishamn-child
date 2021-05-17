5fb980da780cad8583af295cb4160cc91ea40d97
Erik Stenbäcka <Erik.Stenbacka@sigma.se>
2021-05-17 09:56:38 +0200

    SIEW-382 Hide PHP version update warning

--- library/Theme/WordpressAdminConfig.php
+++ library/Theme/WordpressAdminConfig.php
@@ -12,6 +12,7 @@ class WordpressAdminConfig
         add_filter( 'wp_fatal_error_handler_enabled', '__return_false' );
         add_action( 'wp_dashboard_setup', function () {
             remove_meta_box( 'dashboard_site_health', 'dashboard', 'normal' );
+            remove_meta_box( 'dashboard_php_nag', 'dashboard', 'normal' );
         });
         add_action( 'admin_menu', function () {
             remove_submenu_page( 'tools.php','site-health.php' );
